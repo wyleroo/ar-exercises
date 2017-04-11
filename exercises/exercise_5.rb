a56ef723cd7f11f60d8ca801285fdc502dce3fc9
Wyatt Robinson <thewyatt@gmail.com>
2017-04-11 23:36:17 +0000

    Completed exercises 4 & 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+sum =  Store.sum(:annual_revenue)
+avg = sum/Store.count
+winners = Store.where("annual_revenue > 1000000").count
+
+puts sum
+puts avg
+puts winners
